77e563e5075a24759843b69d1b68a7087dbc4a49
CallmePapa <1062927638@qq.com>
2018-06-12 23:20:18 +0800

    :sparkles:second push

--- app/index.js
+++ app/index.js
@@ -1,3 +1,94 @@
 require('../style/style.css');
-import sum from './sum'
-console.log(sum(1,2));
+
+//购物车
+let cart = document.getElementById('cart');
+let cartMenu = document.getElementById('cartMenu');
+
+cart.onmouseover = function () {
+    cart.style.background = 'fff';
+    cart.style.color = '#ff6700';
+    cartMenu.style.height = 100 + 'px'
+};
+
+cart.onmouseleave = function () {
+    cart.style.background = '424242';
+    cart.style.color = '#b0b0b0';
+    cartMenu.style.height = 0 + 'px';
+};
+
+//搜索框
+let searchText = document.getElementById('search-text');
+let searchotwords = document.getElementById('search-hot-words');
+
+searchText.onfocus = function () {
+    searchotwords.style.display = 'none';
+};
+
+searchText.onblur = function () {
+    searchotwords.style.display = 'block';
+};
+
+//轮播
+
+let oDiv = document.getElementById('lunbo');
+let next = document.getElementById('next');
+let pre = document.getElementById('pre');
+let ul = oDiv.getElementsByTagName('ul')[0];
+let uLi = ul.getElementsByTagName('li');
+let ol = oDiv.getElementsByTagName('ol')[0];
+let oLi = ol.getElementsByTagName('li');
+
+let now = 0;
+
+for (let i = 0; i < oLi.length; i++) {
+    oLi[i].index = i;
+    oLi[i].onclick = function () {
+        now = this.index;
+        tab();
+    }
+}
+
+function tab() {
+    for (let j = 0; j < oLi.length; j++) {
+        oLi[j].className = '';
+        move(uLi[j], {opacity: 0})
+    }
+    oLi.className = 'click';
+    move(uLi[now], {opacity: 100});
+}
+
+pre.onclick = function () {
+    now--;
+    if (now === -1) {
+        now = oLi.length - 1;
+    }
+    tab();
+};
+
+next.onclick = function () {
+    now++;
+    if (now === oLi.length) {
+        now = 0;
+    }
+    tab();
+};
+
+let timer0 = setInterval(next.onclick, 3000);
+oDiv.onmouseover = function () {
+    clearInterval(timer0);
+    move(next, {opacity: 100});
+    move(pre, {opacity: 100});
+};
+
+oDiv.onmouseleave=function () {
+    timer0=setInterval(next.onclick,3000);
+    move(next,{opacity:0});
+    move(pre,{opacity:0});
+};
+//没写完
+
+function move(obj,jason,fn) {
+    clearInterval(obj.timer);
+
+}
+
